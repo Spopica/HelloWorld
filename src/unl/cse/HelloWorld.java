aacf74f49ebff35735642c48c1a7dd4c169512f6
Sebastian Letos <sebastianletos@yahoo.com>
2021-03-24 13:39:55 +0200

    Am adaugat bro un print nou ;)

--- src/unl/cse/HelloWorld.java
+++ src/unl/cse/HelloWorld.java
@@ -8,6 +8,8 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("How are you?");
 		System.out.println("Hi I'm dandy, you?");
+		
+		System.out.println("Salut bro, cum merge la PIP? :)");
 	}
 
 }
